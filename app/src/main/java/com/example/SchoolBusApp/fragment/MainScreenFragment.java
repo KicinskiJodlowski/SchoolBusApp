f4b4831180fbb338fc1866117ddc256b92cbfc55
Kamil <kamil.kicinski@gmail.com>
2019-06-05 22:43:16 +0200

    cleaning code

---
+++ app/src/main/java/com/example/SchoolBusApp/fragment/MainScreenFragment.java
@@ -0,0 +1,132 @@
+package com.example.SchoolBusApp.fragment;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.SchoolBusApp.RetrofitClient;
+import com.example.SchoolBusApp.SharedPreferenceManager;
+import com.example.SchoolBusApp.activity.InitialActivity;
+import com.example.SchoolBusApp.activity.LoginActivity;
+import com.example.SchoolBusApp.adapter.EventRecordAdapter;
+import com.example.SchoolBusApp.model.EventModel;
+import com.example.SchoolBusApp.R;
+
+import java.util.ArrayList;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainScreenFragment extends Fragment {
+
+    ListView listViewEvents;
+
+    public static ArrayList<EventModel> listEvents;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_screen_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listViewEvents = getActivity().findViewById(R.id.listViewEvents);
+
+
+//        listEvents = new ArrayList<>();
+//        getEvents();
+    }
+
+
+    private void getEvents() {
+
+        Call<ArrayList<EventModel>> call = RetrofitClient.getInstance().getApi().getEvents(SharedPreferenceManager.read(SharedPreferenceManager.TOKEN, ""));
+        call.enqueue(new Callback<ArrayList<EventModel>>() {
+            @Override
+            public void onResponse(Call<ArrayList<EventModel>> call, Response<ArrayList<EventModel>> response) {
+
+                Log.d("Response Code ", Integer.toString(response.code()));
+                if (response.code() == 200) {
+                    showEvents(response);
+                }
+                else {
+                    Toast.makeText(getActivity(), "Wystąpił błąd! Nie udało się pobrać wydarzeń.", Toast.LENGTH_SHORT).show();
+                    if (response.code() == 401) {
+                        SharedPreferences sharedPreferences;
+                        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(InitialActivity.initialActivity.getApplicationContext());
+                        Call<ResponseBody> logout = new RetrofitClient().getApi().notifyUnregister(sharedPreferences.getString("registrationID",""),
+                                SharedPreferenceManager.read(SharedPreferenceManager.TOKEN, ""));
+                        logout.enqueue(new Callback<ResponseBody>() {
+                            @Override
+                            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                                if (response.code() == 200) {
+                                    //SharedPreferenceManager.remove(SharedPreferenceManager.RegisterID);
+                                    Log.d("unRegID", "Wyrejestrowanie z usługi udane");
+                                } else Log.d("unRegID", "Wyrejestrowanie z usługi nie powiodło się");
+                            }
+
+                            @Override
+                            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                                Log.d("unRegID", "Failure request");
+                            }
+                        });
+                        SharedPreferenceManager.remove(SharedPreferenceManager.TOKEN);
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<EventModel>> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void showEvents(Response<ArrayList<EventModel>> response) {
+        Toast.makeText(getActivity(), "Pobrano wydarzenia", Toast.LENGTH_SHORT).show();
+        ArrayList<EventModel> events = response.body();
+        listEvents.addAll(events);
+
+        EventRecordAdapter adapter = new EventRecordAdapter(getActivity(), R.layout.event_record, listEvents);
+        listViewEvents.setAdapter(adapter);
+        listViewEvents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                showEventDetails(position);
+            }
+        });
+    }
+
+    private void showEventDetails(int position) {
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        EventDetailsFragment detailsFragment = new EventDetailsFragment();
+
+        Bundle arg = new Bundle();
+        arg.putSerializable("event", listEvents.get(position));
+        detailsFragment.setArguments(arg);
+
+        ft.replace(R.id.fragment_container, detailsFragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+}
---
+++ app/src/main/java/com/example/SchoolBusApp/fragment/RegisterFragment.java
@@ -0,0 +1,149 @@
+package com.example.SchoolBusApp.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.SchoolBusApp.RetrofitClient;
+import com.example.SchoolBusApp.SharedPreferenceManager;
+import com.example.SchoolBusApp.activity.MainActivity;
+import com.example.SchoolBusApp.model.UserJSONModel;
+import com.example.SchoolBusApp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterFragment extends Fragment {
+
+    private Intent activityIntent;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.register_fragment, container, false);
+        return rootView;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button loginViewButton = view.findViewById(R.id.signInButton);
+        Button registerUserButton = view.findViewById(R.id.loginButton);
+        final EditText password = view.findViewById(R.id.passwordText);
+        EditText passwordRepeat = view.findViewById(R.id.passwordRepeatText);
+        final Editable passwordText = password.getText();
+        final Editable passwordRepeatText = passwordRepeat.getText();
+        final TextView errorText = getActivity().findViewById(R.id.errorText);
+
+        loginViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerLogin, new LoginFragment()).commit();
+            }
+        });
+        registerUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+                if (passwordRepeatText.toString().equals(passwordText.toString())) {
+                    registerOnClick();
+                } else errorText.setText("Podane hasła nie są tożsame.");
+            }
+        });
+    }
+
+    private void registerOnClick() {
+//        UserJSONModel user = new UserJSONModel(((EditText) getActivity().findViewById(R.id.userNameText)).getText().toString(),
+//                ((EditText) getActivity().findViewById(R.id.mailText)).getText().toString(),
+//                ((EditText) getActivity().findViewById(R.id.passwordText)).getText().toString(),
+//                ((EditText) getActivity().findViewById(R.id.fullNameText)).getText().toString(), "");
+        final TextView errorText = getActivity().findViewById(R.id.errorText);
+        Call<ResponseBody> call = RetrofitClient.getInstance().getApi().register(SharedPreferenceManager.read(SharedPreferenceManager.MASTER_KEY, ""),
+                ((EditText) getActivity().findViewById(R.id.mailText)).getText().toString(), ((EditText) getActivity().findViewById(R.id.passwordText)).getText().toString());
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                switch (response.code()) {
+                    case 201:
+                        try {
+                            SharedPreferenceManager.write(SharedPreferenceManager.AUTH, response.body().string());
+                        } catch (IOException e) {
+                            Toast.makeText(getActivity(), "Server internal error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        try {
+                            SharedPreferenceManager.write(SharedPreferenceManager.TOKEN, new JSONObject(SharedPreferenceManager.read(SharedPreferenceManager.AUTH, "")).getString("token"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            UserJSONModel user = new UserJSONModel(new JSONObject(SharedPreferenceManager.read(SharedPreferenceManager.AUTH, "")).getJSONObject("user"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        activityIntent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(activityIntent);
+                        getActivity().finish();
+                        break;
+                    case 400:
+                        errorText.setText("");
+                        try {
+                            errorText.append("Błąd rejestracji:\n"+new JSONObject(response.errorBody().string()).getString("message") + "\n");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case 401:
+                        errorText.setText("");
+                        errorText.append("Server internal error: Master_token is invalid.");
+                        break;
+                    case 409:
+                        errorText.setText("");
+                        try {
+                            errorText.append("Błąd rejestracji:\n"+new JSONObject(response.errorBody().string()).getString("message") + "\n");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+//                List<Error> errors = response.body().getErrors();
+//                if(errors.size()==0){
+//                errorText.setText("");
+//                for (Error e : errors) {
+//                    errorText.append(e.getDescription()+ "\n");
+//                }}else {
+//                    Toast.makeText(getActivity(),"Pomyślnie zarejestrowano użytkownika - możesz się teraz zalogować.", Toast.LENGTH_LONG).show();
+//                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerLogin, new LoginFragment()).commit();
+//                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(getActivity(), "Rejestracja nie powiodła się.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
